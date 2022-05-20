7c4bc3d7f5ce028f00a6278e860682d77b2696fa
Seth Hammell <sethjhammell@gmail.com>
2022-05-20 01:50:06 -0400

    cleaned up UI

--- client/src/home/home.tsx
+++ client/src/home/home.tsx
@@ -1,21 +1,20 @@
-import { Button, TextField } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { Button, Card, TextField } from "@mui/material";
+import React, { useState } from "react";
+import "./home.css";
 
 function Home() {
   const api = "/api/playlists/";
   const searchString = "list=";
   const [url, setUrl] = useState("");
 
-  useEffect(() => {}, []);
-
   function fetchRemovedVideos() {
     const index = url.indexOf(searchString) + searchString.length;
 
     // -1 + 5 = 4
     if (index === 4) return;
-    
-    const id = url.substring(index, url.length)
-    console.log(encodeURIComponent(id))
+
+    const id = url.substring(index, url.length);
+    console.log(encodeURIComponent(id));
     fetch(`${api}${id}`)
       .then((res) => res.json())
       .then((data) => {
@@ -30,16 +29,22 @@ function Home() {
   return (
     <div>
       {/* drop down of actions: print all videos, blocked videos, scrape wayback */}
-      <TextField
-        value={url}
-        onChange={updateUrl}
-        autoFocus
-        margin="dense"
-        label="Playlist URL"
-      />
-      <Button variant="contained" onClick={fetchRemovedVideos}>
-        Fetch
-      </Button>
+      <div className="header-wrapper">
+        <Card className="welcome-header">Playlist Preservation</Card>
+      </div>
+      <div className="input">
+        <div className="url">
+          <TextField
+            value={url}
+            onChange={updateUrl}
+            autoFocus
+            label="Playlist URL"
+          />
+        </div>
+        <Button variant="contained" onClick={fetchRemovedVideos}>
+          Find Removed Videos
+        </Button>
+      </div>
     </div>
   );
 }
